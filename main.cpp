6cee41dac0b68ad797c1da4fa375bdbc5190318b
Andy Liu <57054829+AndyLiu314@users.noreply.github.com>
2026-02-23 16:19:09 -0800

    Added debug layer to project

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+
 #include "Support/CustomWin.h"
 #include "Support/ComPointer.h"
+#include "DXDebug/DebugLayer.h"
 
 int main()
 {
+	DXDebugLayer::Get().Init(); 
 
+	DXDebugLayer::Get().Shutdown();
 }
